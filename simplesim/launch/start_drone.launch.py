237c32d377163ae4b6c94ba7defef68177bafb48
Anastasia Muran Trus <anastasiamurantrus@gmail.com>
2024-02-19 12:57:48 +0100

    creando launch substitution

--- simplesim/launch/start_drone.launch.py
+++ simplesim/launch/start_drone.launch.py
@@ -1,45 +1,38 @@
 import launch
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    config_file = 'mult_config.rviz'
-    config = os.path.join(
-        get_package_share_directory('simplesim'),
-        'rviz',
-        config_file
+    drone_id = LaunchConfiguration('drone_id', default='drone_x')
+
+    drone_id_launch_arg = DeclareLaunchArgument(
+        'drone_id',
+        default_value='drone_x'
     )
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d' + config]
-    )
-    drone_name = 'drone_1'
     fp = launch_ros.actions.Node(
-        namespace=drone_name,
+        namespace=drone_id,
         package='simplesim',
         executable='fp',
         name='fp'
     )
     simulator = launch_ros.actions.Node(
-        namespace=drone_name,
+        namespace=drone_id,
         package='simplesim',
         executable='simulator',
         name='simulator'
-    
+    )
     wps = launch_ros.actions.Node(
-        namespace=drone_name,
+        namespace=drone_id,
         package='simplesim',
         executable='wp_vis',
         name='wps'
-)
+    )
 
     return launch.LaunchDescription([
-        rviz_node,
+        drone_id_launch_arg,
         fp,
         simulator,
         wps
